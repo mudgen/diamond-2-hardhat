d0d1d0520695cb42ee5460c6f1268f43150b5d7d
Jake <jake.mc@icloud.com>
2024-05-02 12:13:22 +1000

    undo code formatter changes

--- contracts/libraries/LibDiamond.sol
+++ contracts/libraries/LibDiamond.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 * Author: Nick Mudge <nick@perfectabstractions.com> (https://twitter.com/mudgen)
 * EIP-2535 Diamonds: https://eips.ethereum.org/EIPS/eip-2535
 /******************************************************************************/
-import {IDiamondCut} from "../interfaces/IDiamondCut.sol";
+import { IDiamondCut } from "../interfaces/IDiamondCut.sol";
 
 // Remember to add the loupe functions from DiamondLoupeFacet to the diamond.
 // The loupe functions are required by the EIP2535 Diamonds standard
@@ -67,7 +67,11 @@ library LibDiamond {
     // 'Facet[] calldata _diamondCut'.
     // The code is duplicated to prevent copying calldata to memory which
     // causes an error for a two dimensional array.
-    function diamondCut(IDiamondCut.FacetCut[] memory _diamondCut, address _init, bytes memory _calldata) internal {
+    function diamondCut(
+        IDiamondCut.FacetCut[] memory _diamondCut,
+        address _init,
+        bytes memory _calldata
+    ) internal {
         DiamondStorage storage ds = diamondStorage();
         uint256 originalSelectorCount = ds.selectorCount;
         uint256 selectorCount = originalSelectorCount;
@@ -172,7 +176,7 @@ library LibDiamond {
                 } else {
                     selectorInSlotIndex--;
                 }
-                // The below code fixes the above issue
+                // Repalcing the above with the below code fixes the issue
                 // if (_selectorSlot != 0 || (selectorInSlotIndex != 0 && _selectors[selectorIndex] == 0)) {
                 //     selectorInSlotIndex--;
                 // } else {
